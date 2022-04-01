1f99cc2526a4aa8717c7522a595b5ace859e9f46
Caio Cavalcanti <caio-cavalcanti@hotmail.com>
2022-04-01 19:03:49 -0300

    fix footer responsive

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,17 +1,21 @@
 import React from "react";
+import './Footer.css'
 
-import styles from "./styles.module.css";
+const Footer = () => {
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className={styles.footer} >
-        <p className={styles.text}>
-          Feito Por Caio Cavalcanti {new Date().getFullYear()} - Todos direitos reservados
-        </p>
-      </footer>
-    );
-  }
+  return (
+    <footer className="footer">
+      <div className="footer__social">
+        <a href="https://www.instagram.com/speak.up.tech/">
+
+        </a>
+        <a href="https://www.youtube.com/c/SpeakupTech">
+
+        </a>  
+      </div>
+      <p className="footer__copyright">Feito Por Caio Cavalcanti</p>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -83,7 +83,7 @@ const Login = () => {
                     Login</button>
 
 
-                    {/* <button><a target="_blank" href='http://localhost:3000/dashbord'>Click me!</a> </button> */}
+                   
                 </Form>
             </Formik>
         </div>
@@ -93,24 +93,3 @@ const Login = () => {
 export default Login;
 
 
-// function Login(props){
-    
-//     return (
-    
-//     <form className="form" onSubmit={props.handleRegister}>
-//         <div id="marginTop"className="imgContainer center">
-//             <img src="https://elogroup.com.br/wp-content/uploads/2020/09/logo-EloGroup-branco.png" alt="logo da Elogroup"></img>
-//         </div>
-//         <p className ="inputText textCenter">Username:</p>
-//         <input className="center input" type="text" required="required" value = {props.username} onChange={props.setUsername}></input>
-//         <p className ="inputText textCenter">Password:</p>
-//         <input className="center input" onKeyUp={props.validatePassword} type="password" pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@+#$])[a-zA-Z0-9@+#$]{8,50}$" title="Password deve possuir ao menos 8 caracteres, contendo ao menos, um caracter especial(@+#$), um caracter numérico, um caracter alfanumérico;" required="required" minlength="8" placeholder="********" value = {props.password} onChange={props.setPassword}></input>
-//         <p className ="inputText textCenter">Confirmar Password:</p>
-//         <input className="center input" onKeyUp={props.validatePassword} id="confirm_password" type="password" required="required" minlength="8" placeholder="********" value = {props.confirmPassword} onChange={props.setConfirmPassword}></input>
-//         <br/>
-//         <button className="salveLead" type="submit" className="buttonRegister">Confirm</button>
-//     </form>
-//     )
-// }
-
-// export default Login;
