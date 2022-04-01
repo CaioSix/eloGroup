e5b444d2ee2156c5d1b55613bc76e9dc7bef7fef
Caio Cavalcanti <caio-cavalcanti@hotmail.com>
2022-04-01 19:06:50 -0300

    fix footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,14 +5,7 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div className="footer__social">
-        <a href="https://www.instagram.com/speak.up.tech/">
-
-        </a>
-        <a href="https://www.youtube.com/c/SpeakupTech">
-
-        </a>  
-      </div>
+     
       <p className="footer__copyright">Feito Por Caio Cavalcanti</p>
     </footer>
   )
